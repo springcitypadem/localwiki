a4e39f34acd399830f0191c3453154fb046602e9
Ryan Tucker <rtucker@gmail.com>
2011-07-30 12:06:01 -0400

    comments: add better __unicode__ repr to model

--- sapling/comments/models.py
+++ sapling/comments/models.py
@@ -3,3 +3,9 @@ from versionutils.versioning import TrackChanges
 
 class VersionedComment(Comment):
     history = TrackChanges()
+
+    class Meta:
+        verbose_name = 'comment'
+
+    def __unicode__(self):
+        return u'Comment by %s on %r: %s...' % (self.name, self.content_object, self.comment[:50])
